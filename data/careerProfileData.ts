8ab694863da3dde22748a2be60c737f2cb0a01f9
alpha73-admin <shinsuke.aso.tu4@gmail.com>
2026-03-02 00:45:47 +0900

    ♻️Links.tsxにて改行する処理を追加

--- data/careerProfileData.ts
+++ data/careerProfileData.ts
@@ -22,9 +22,7 @@ export const careerProfileData: ProfileData = {
     "入社後建築企業向け業務システムや塗装企業向けの基幹システムの構築など主にバックエンド側のシステム開発に従事。",
     "現在はフロントエンドおよびAI技術について学習中。",
   ],
-  wishToDo: [
-    "TODO",
-  ],
+  wishToDo: ["TODO"],
   expertise: [
     "フルスタック",
     "バックエンド",
@@ -32,17 +30,24 @@ export const careerProfileData: ProfileData = {
     "AI",
     "C#",
     "TypeScript",
+    "next.js",
+    "nestJS",
+    "Hono(学習中)",
+    "ElysiaJS",
+    "React(学習中)",
+    "詳細設計",
     "データベース設計",
     "API設計",
-    "Kiro",
+    "AI駆動開発",
     "Antigravity",
+    "Kiro",
     "SDD",
   ],
   experience: [
     {
       company: "WONQ 株式会社",
       position: "システムエンジニア",
-      period: "2025年12月 - 現在",
+      period: "2024年12月 - 現在",
       description: [
         {
           text: "建築企業向け業務システムの構築",
@@ -53,9 +58,7 @@ export const careerProfileData: ProfileData = {
         },
         {
           text: "塗装企業向けの基幹システムの構築",
-          details: [
-            "コードレビュー文化の導入により、バグ検出率が30%向上",
-          ],
+          details: ["コードレビュー文化の導入により、バグ検出率が30%向上"],
         },
         {
           text: "主にバックエンド側のシステム開発に従事",
@@ -64,17 +67,25 @@ export const careerProfileData: ProfileData = {
           text: "フロントエンド技術の学習・適用",
         },
       ],
-      technologies: ["TypeScript", "データベース設計", "API設計", "Kiro", "Antigravity", "SDD", "フロントエンド"],
+      technologies: [
+        "TypeScript",
+        "データベース設計",
+        "API設計",
+        "Kiro",
+        "Antigravity",
+        "SDD",
+        "フロントエンド",
+      ],
     },
     {
       company: "カナテック 株式会社",
       position: "プログラマ",
-      period: "2022年4月 - 2025年8月",
+      period: "2022年4月 - 2024年7月",
       description: [
         {
           text: "主にバックエンド側のシステム開発に従事",
           details: [
-            "主にバックエンド側のシステム開発に従事",
+            "WindowsOSやLinuxOSに対してVBで書かれたFAシステムをC#にリプレースする案件に最も多く携わりました。\n工場に赴いて新規システムを開発・導入した経験もございます。",
           ],
         },
       ],
@@ -85,21 +96,23 @@ export const careerProfileData: ProfileData = {
     {
       name: "GitHub",
       url: "https://github.com/albeegamengine",
-      description: "技術的なプロジェクトとコード",
+      description: "技術的なプロジェクトとコードです。",
       isExternal: true,
       category: "portfolio",
     },
     {
       name: "個人開発用ブログ",
       url: "https://linealbeegames4730.blogspot.com/",
-      description: "個人開発用ブログ(Blogger)のリンクです。",
+      description:
+        "個人開発用ブログ(Blogger)のリンクです。\n技術発信の練習のために記事を定期的に公開しています。",
       isExternal: true,
       category: "career",
     },
     {
       name: "YouTube",
       url: "https://www.YouTube.com/@albeegamengine",
-      description: "個人開発用YouTubeのリンクです。",
+      description:
+        "個人開発用YouTubeのリンクです。\n技術発信の練習のため手始めに趣味であるグランブルーファンタジーの動画をアップロードしてみました。",
       isExternal: true,
       category: "career",
     },
